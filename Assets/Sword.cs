011c750e9eccc7f2e41a112349b2509bf56b5328
Moro-production <152307406+Moro-production@users.noreply.github.com>
2023-12-14 07:18:03 -0800

    .

--- Assets/Sword.cs
+++ Assets/Sword.cs
@@ -10,20 +10,20 @@ public class Sword : MonoBehaviour
     public Animator anim;
     //public GameObject Enemy1;
     public int SwordDmg=10;
+  
+
 
     void Start()
     {
-        anim = gameObject.GetComponent<Animator>();
+       
+       
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            anim.SetTrigger("Active");
-        }
-        
+       
+
     }
-   
+
 
 
 }
